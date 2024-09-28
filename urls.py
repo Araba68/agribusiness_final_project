ad37bbce901b20447836b392fe6536653721ecd9
Araba68 <58734699+Araba68@users.noreply.github.com>
2024-09-28 19:50:23 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,26 +1,28 @@
-"""
-URL configuration for mkulima project.
+# portal/urls.py
+from django.urls import path
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from portal import views  # Import your views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+from .views import farmer_dashboard, contact_admin, access_finances
+from .views import update_farmer_profile, update_buyer_profile
+from django.views.generic import TemplateView
+#from django.conf import settings
+#from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Route for the home page
-    path('portal/', include('portal.urls')), 
-   
+    path('signin/', views.signin, name='signin'),  # Sign In page URL
+      path('signup_buyer/', views.signup_buyer, name='signup_buyer'),
+    path('signup_farmer/', views.signup_farmer, name='signup_farmer'),
+    path('buyer_dashboard/', views.buyer_dashboard, name='buyer_dashboard'),
+    path('farmer_dashboard/', views.farmer_dashboard, name='farmer_dashboard'),
+     path('update_buyer_profile/', views.update_buyer_profile, name='update_buyer_profile'),
+      path('update_farmer_profile/', views.update_farmer_profile, name='update_farmer_profile'),
+       path('logout/', LogoutView.as_view(next_page='signin'), name='logout'),  # Update this line
+        path('contact_admin/', contact_admin, name='contact_admin'),
+    path('access_finances/', access_finances, name='access_finances'),
+     
+   path('error/', TemplateView.as_view(template_name='portal/error.html'), name='error')
+      
+
 ]
